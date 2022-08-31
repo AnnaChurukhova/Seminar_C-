dcefa1fd9bab1ec5ee304a27547dec2a5affdff3
AnnaChurukhova <anechkin@bk.ru>
2022-08-31 15:13:46 +0300

    Задача 8

---
+++ S_8/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a=1;
+while (a <= number){
+    if (a%2 ==0){
+System.Console.WriteLine(a);
+    }
+    a++;
+}
