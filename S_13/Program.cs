8317acb3dc04ef0a9ab3f0429d042358430c26a9
AnnaChurukhova <anechkin@bk.ru>
2022-09-07 12:44:03 +0300

    Задача 13

---
+++ S_13/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine ("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+char [] arrNum = num.ToString().ToCharArray();
+if (arrNum.Length > 2) {
+    System.Console.WriteLine(arrNum[arrNum.Length - 3]);
+}
+else {
+    System.Console.WriteLine("Третьей цифры нет");
+}
