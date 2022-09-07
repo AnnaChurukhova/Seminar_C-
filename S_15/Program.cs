c91ec7eb2fdc6ce0fefdbf202a5899e4af4a0567
AnnaChurukhova <anechkin@bk.ru>
2022-09-07 12:52:03 +0300

    Задача 15

---
+++ S_15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 6 && num > 0) {
+    Console.WriteLine("Не выходной");
+}
+else if (num > 5 && num < 8) {
+    Console.WriteLine("Выходной");
+}
+else {
+    Console.WriteLine("Этого дня недели не существует");
+}
